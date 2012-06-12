c2a03ce5331d4c01b67abce8836cb0d048612313
unknown <Desktop@Desktop-PC.(none)>
2012-06-12 11:44:33 +0200

    adding DS

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,5 +1,10 @@
 dataSource {
+    pooled = false
     jndiName = "java:comp/env/GrailsDB"
+    /*pooled = true
+    driverClassName = "org.h2.Driver"
+    username = "sa"
+    password = ""*/
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -7,34 +12,24 @@ hibernate {
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
 // environment specific settings
+
 environments {
-    development {
+  development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            dbCreate = "create-drop"
+            url = "jdbc:h2:mem:devDb"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:h2:mem:testDb"
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
+            url = "jdbc:h2:prodDb"
         }
     }
 }
